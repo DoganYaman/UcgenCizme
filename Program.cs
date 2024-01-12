1f18e392fb671f230b4549b34956ed2a7296add1
Dogan Yaman <yamandogan963@gmail.com>
2024-01-12 23:02:01 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ucgen_cizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgen yüksekliğini giriniz : ");
+            int yukseklik = int.Parse(Console.ReadLine());
+            UcgenCiz(yukseklik);
+        }
+
+        static void UcgenCiz(int yukseklik)
+        {
+            for (int i = 0; i < yukseklik; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
